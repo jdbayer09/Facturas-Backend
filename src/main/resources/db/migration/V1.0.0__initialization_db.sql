34524aed095c9e9c0f813d9961372ea4b2270555
Julian David Bayer <jdbayer09@hotmail.com>
2026-01-26 16:21:02 -0500

    creacion de tabla de usuarios

--- src/main/resources/db/migration/V1.0.0__initialization_db.sql
+++ src/main/resources/db/migration/V1.0.0__initialization_db.sql
@@ -1,1 +1,13 @@
 create schema core;
+create schema security;
+
+create table security.users (
+    id uuid not null primary key,
+    name varchar(60) not null,
+    last_name varchar(60) not null,
+    email varchar(100) not null constraint user_unique_email unique,
+    password varchar(500) not null,
+    is_active boolean default false not null,
+    created_at  timestamp not null,
+    updated_at  timestamp not null
+);
